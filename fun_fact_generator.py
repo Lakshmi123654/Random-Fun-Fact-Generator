e139c71eaa515b7b4fa9a887ab9284db5f1df6fe
Dhivya Lakshmi <162443022+Lakshmi123654@users.noreply.github.com>
2024-03-09 15:02:31 +0530

    Create fun_fact_generator.py ; first commit
    A fun, efficient, every seven ( 7 ) minute Random Fun Fact Generator  to keep your mind happy ! 
    Created BY MR. V. BHAVISH JAITHRA
    Works perfectly fine !

---
+++ fun_fact_generator.py
@@ -0,0 +1,27 @@
+import randfacts
+import winsound
+import time
+from time import sleep
+import os
+from plyer import notification
+
+def send_notification (title, message) :
+
+    notification.notify (
+
+        title = "Random Fun Fact !!",
+        message = f"{x}",
+        timeout = 17
+	)
+
+if __name__ == "__main__" :
+
+    os.system ("cls")
+
+    x = str (randfacts.get_fact ().title ())
+
+    while True :
+
+        sleep (420)
+
+        send_notification ("Random Fun Fact !!".title (), f"\n{x}".title ())
